eb35a86b026489296d79bafceb60e5ed476947d1
binarydigitz01 <binarydigitz01@protonmail.com>
2024-09-15 19:23:07 +0530

    Updated navbar

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,6 +1,17 @@
+import Image from 'next/image';
+import logo from './images/logo.jpg'
+
 export default function header() {
   return (
     <header>
-  <a href="/about">Info</a>
-    </header>)
+      <nav>
+        <div id="navbar_logo">
+          <Image src={logo} width={50} height={50} />
+        </div>
+        <ul id="navbar_links">
+          <li class="navbar_item"><a href="/">Home</a></li>
+          <li class="navbar_item"><a href="/about">Info</a></li>
+        </ul>
+      </nav>
+    </header>);
 }
