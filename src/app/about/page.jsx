9832716c1f1536f8c410d2ec707be72ff4dba3ef
binarydigitz01 <binarydigitz01@protonmail.com>
2024-09-15 19:23:27 +0530

    Incomplete about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,9 @@
 export default function About(){
-  return (<h1>Hello!</h1>);
+  return (
+    <div>
+      <h1>About Us – MAHE Model United Nations </h1>
+
+      <p>Welcome to the MAHE version of the Model United Nations, the first MUN conference hosted by Manipal Academy of higher education (MAHE). This prestigious event marks MAHE’s inaugural step into the sector of version United international locations, inviting students from throughout the globe to have interaction in diplomatic simulations, debates, and coverage discussions.</p>
+    </div>
+  );
 }
